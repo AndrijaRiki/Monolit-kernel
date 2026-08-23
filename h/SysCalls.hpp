2cdaf3701698985193864d0372862bd75b7bf601
Andrija Ristić <106862464+AndrijaRiki@users.noreply.github.com>
2026-08-23 14:49:22 -0700

    Add files via upload

--- h/SysCalls.hpp
+++ h/SysCalls.hpp
@@ -1,3 +1,285 @@
+#ifndef PROJEKAT_SYSCALL_HPP
+#define PROJEKAT_SYSCALL_HPP
+
+#include "../lib/hw.h"
+
+class SysCalls
+{
+public:
+
+    // =========================================================
+    // invoke<T> - 0 ARGUMENATA
+    // =========================================================
+    template <typename RetType>
+    static inline RetType invoke(uint64 id)
+    {
+        uint64 ret;
+
+        asm volatile(
+            "mv a0, %1\n"
+            "ecall\n"
+            "mv %0, a0\n"
+            : "=r"(ret)
+            : "r"(id)
+            : "a0", "memory"
+        );
+
+        return (RetType)ret;
+    }
+
+
+    // =========================================================
+    // invoke<T> - 1 ARGUMENT
+    // =========================================================
+    template <typename RetType, typename Arg1>
+    static inline RetType invoke(uint64 id, Arg1 arg1)
+    {
+        uint64 ret;
+
+        asm volatile(
+            "mv a0, %1\n"
+            "mv a1, %2\n"
+            "ecall\n"
+            "mv %0, a0\n"
+            : "=r"(ret)
+            : "r"(id),
+              "r"((uint64)arg1)
+            : "a0", "a1", "memory"
+        );
+
+        return (RetType)ret;
+    }
+
+
+    // =========================================================
+    // invoke<T> - 2 ARGUMENTA
+    // =========================================================
+    template <typename RetType, typename Arg1, typename Arg2>
+    static inline RetType invoke(uint64 id, Arg1 arg1, Arg2 arg2)
+    {
+        uint64 ret;
+
+        asm volatile(
+            "mv a0, %1\n"
+            "mv a1, %2\n"
+            "mv a2, %3\n"
+            "ecall\n"
+            "mv %0, a0\n"
+            : "=r"(ret)
+            : "r"(id),
+              "r"((uint64)arg1),
+              "r"((uint64)arg2)
+            : "a0", "a1", "a2", "memory"
+        );
+
+        return (RetType)ret;
+    }
+
+
+    // =========================================================
+    // invoke<T> - 3 ARGUMENTA
+    // =========================================================
+    template <
+        typename RetType,
+        typename Arg1,
+        typename Arg2,
+        typename Arg3
+    >
+    static inline RetType invoke(
+        uint64 id,
+        Arg1 arg1,
+        Arg2 arg2,
+        Arg3 arg3
+    )
+    {
+        uint64 ret;
+
+        asm volatile(
+            "mv a0, %1\n"
+            "mv a1, %2\n"
+            "mv a2, %3\n"
+            "mv a3, %4\n"
+            "ecall\n"
+            "mv %0, a0\n"
+            : "=r"(ret)
+            : "r"(id),
+              "r"((uint64)arg1),
+              "r"((uint64)arg2),
+              "r"((uint64)arg3)
+            : "a0", "a1", "a2", "a3", "memory"
+        );
+
+        return (RetType)ret;
+    }
+
+
+    // =========================================================
+    // invoke<T> - 4 ARGUMENTA
+    // =========================================================
+    template <
+        typename RetType,
+        typename Arg1,
+        typename Arg2,
+        typename Arg3,
+        typename Arg4
+    >
+    static inline RetType invoke(
+        uint64 id,
+        Arg1 arg1,
+        Arg2 arg2,
+        Arg3 arg3,
+        Arg4 arg4
+    )
+    {
+        uint64 ret;
+
+        asm volatile(
+            "mv a0, %1\n"
+            "mv a1, %2\n"
+            "mv a2, %3\n"
+            "mv a3, %4\n"
+            "mv a4, %5\n"
+            "ecall\n"
+            "mv %0, a0\n"
+            : "=r"(ret)
+            : "r"(id),
+              "r"((uint64)arg1),
+              "r"((uint64)arg2),
+              "r"((uint64)arg3),
+              "r"((uint64)arg4)
+            : "a0", "a1", "a2", "a3", "a4", "memory"
+        );
+
+        return (RetType)ret;
+    }
+
+
+    // =========================================================
+    // invokeVoid - 0 ARGUMENATA
+    // =========================================================
+    static inline void invokeVoid(uint64 id)
+    {
+        asm volatile(
+            "mv a0, %0\n"
+            "ecall\n"
+            :
+            : "r"(id)
+            : "a0", "memory"
+        );
+    }
+
+
+    // =========================================================
+    // invokeVoid - 1 ARGUMENT
+    // =========================================================
+    template <typename Arg1>
+    static inline void invokeVoid(uint64 id, Arg1 arg1)
+    {
+        asm volatile(
+            "mv a0, %0\n"
+            "mv a1, %1\n"
+            "ecall\n"
+            :
+            : "r"(id),
+              "r"((uint64)arg1)
+            : "a0", "a1", "memory"
+        );
+    }
+
+
+    // =========================================================
+    // invokeVoid - 2 ARGUMENTA
+    // =========================================================
+    template <typename Arg1, typename Arg2>
+    static inline void invokeVoid(
+        uint64 id,
+        Arg1 arg1,
+        Arg2 arg2
+    )
+    {
+        asm volatile(
+            "mv a0, %0\n"
+            "mv a1, %1\n"
+            "mv a2, %2\n"
+            "ecall\n"
+            :
+            : "r"(id),
+              "r"((uint64)arg1),
+              "r"((uint64)arg2)
+            : "a0", "a1", "a2", "memory"
+        );
+    }
+
+
+    // =========================================================
+    // invokeVoid - 3 ARGUMENTA
+    // =========================================================
+    template <
+        typename Arg1,
+        typename Arg2,
+        typename Arg3
+    >
+    static inline void invokeVoid(
+        uint64 id,
+        Arg1 arg1,
+        Arg2 arg2,
+        Arg3 arg3
+    )
+    {
+        asm volatile(
+            "mv a0, %0\n"
+            "mv a1, %1\n"
+            "mv a2, %2\n"
+            "mv a3, %3\n"
+            "ecall\n"
+            :
+            : "r"(id),
+              "r"((uint64)arg1),
+              "r"((uint64)arg2),
+              "r"((uint64)arg3)
+            : "a0", "a1", "a2", "a3", "memory"
+        );
+    }
+
+
+    // =========================================================
+    // invokeVoid - 4 ARGUMENTA
+    // =========================================================
+    template <
+        typename Arg1,
+        typename Arg2,
+        typename Arg3,
+        typename Arg4
+    >
+    static inline void invokeVoid(
+        uint64 id,
+        Arg1 arg1,
+        Arg2 arg2,
+        Arg3 arg3,
+        Arg4 arg4
+    )
+    {
+        asm volatile(
+            "mv a0, %0\n"
+            "mv a1, %1\n"
+            "mv a2, %2\n"
+            "mv a3, %3\n"
+            "mv a4, %4\n"
+            "ecall\n"
+            :
+            : "r"(id),
+              "r"((uint64)arg1),
+              "r"((uint64)arg2),
+              "r"((uint64)arg3),
+              "r"((uint64)arg4)
+            : "a0", "a1", "a2", "a3", "a4", "memory"
+        );
+    }
+};
+
+#endif // PROJEKAT_SYSCALL_HPP
+
+/*
 #ifndef PROJEKAT_SYSCALLS_HPP
 #define PROJEKAT_SYSCALLS_HPP
 
@@ -59,4 +341,4 @@ public:
     }
 };
 
-#endif //PROJEKAT_SYSCALLS_HPP
+#endif //PROJEKAT_SYSCALLS_HPP*/
